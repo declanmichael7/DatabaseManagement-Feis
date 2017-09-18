ee92258b8de30bda9b3ef938e2fab1de609ec96c
declanmichael7 <31606660+declanmichael7@users.noreply.github.com>
2017-09-18 00:10:11 -0400

    Lab 3
    Declan Byrne
    Lab 3
    9/18

---
+++ Declan_Byrne_Lab_3.sql
@@ -0,0 +1,41 @@
+﻿
+
+SELECT  Orders.ordno, Orders.totalUSD
+FROM Orders;
+
+SELECT Agents.name, Agents.city
+FROM Agents
+WHERE Agents.name = 'Smith';
+
+SELECT Products.pid, Products.name, Products.priceUSD
+FROM Products
+WHERE Products.qty>200010;
+
+SELECT Customers.name, Customers.city
+FROM Customers
+WHERE Customers.city='Duluth';
+
+SELECT Agents.name
+FROM Agents
+WHERE Agents.city <> 'New York' 
+AND Agents.city <> 'Duluth';
+
+SELECT *
+FROM Products
+WHERE Products.city <> 'Dallas'
+AND Products.city <> 'Duluth'
+AND Products.priceUSD>=1.00;
+
+SELECT *
+FROM Orders
+WHERE Orders.month = 'Mar'
+OR Orders.month = 'May';
+
+SELECT *
+FROM Orders
+WHERE Orders.month='Feb'
+AND Orders.totalUSD>=500.00;
+
+SELECT Orders.ordno
+FROM Orders
+WHERE Orders.cid='C005';
