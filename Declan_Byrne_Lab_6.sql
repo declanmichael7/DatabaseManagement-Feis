0eae0ab7ac09544e5924aa413106fdc7192f4536
declanmichael7 <31606660+declanmichael7@users.noreply.github.com>
2017-10-18 00:52:57 -0400

    Declan_Byrne_Lab_6
    Added #4 and #5

--- Declan_Byrne_Lab_6.sql
+++ Declan_Byrne_Lab_6.sql
@@ -27,8 +27,26 @@ Order by p.name DESC;
 SELECT c.name, sum(o.totalUSD) as total, o.pid
 FROM Orders as O, Customers as C
 where o.cid=c.cid
-GROUP BY c.name, o.pid
+GROUP BY c.name, o.cid, o.pid
 Order by total desc;
 
 --4--
+--Not sure why this doesn't work, I can't seem to get Weyland to appear--
 
+SELECT c.name, sum(coalesce(o.totalUSD, 0)) as total
+FROM Orders as O, Customers as C
+where o.cid=c.cid
+GROUP BY c.name, c.cid
+Order by c.name asc;
+
+--5--
+--This one seemed a little too easy, so I feel like it's wrong
+
+Select c.name, p.name, a.name
+From Customers as C, Agents as A, Products as P, Orders as O
+Where c.cid=o.cid
+And p.pid=o.pid
+And a.aid=o.aid
+And a.city='Newark'; 
+
+--6--
