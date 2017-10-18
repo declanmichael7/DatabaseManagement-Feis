9964d78005cb4002e435d0989ba894672ed93e37
declanmichael7 <31606660+declanmichael7@users.noreply.github.com>
2017-10-18 00:16:21 -0400

    Create Declan_Byrne_Lab_6.sql
    First draft

---
+++ Declan_Byrne_Lab_6.sql
@@ -0,0 +1,26 @@
+--Declan Byrne--
+--Lab 6--
+
+--1--
+
+Select c.name, c.city
+From Customers as C, (
+		Select p.city, count(*) as product_count
+		From Products as p
+		Group by (p.city)
+		Order by product_count desc
+		limit 2
+		) as Cities
+Where C.city=Cities.city;
+
+--2--
+
+Select p.name
+From Products as P
+Where p.priceUSD >= (
+    Select avg(priceUSD)
+    From Products AS P)
+Order by p.name DESC;
+    
+--3--
+
